1634e6a24742c7dbd31028ef5dd040cb687d48a6
j34sy <j34sy@proton.me>
2024-07-20 00:39:38 +0200

    sync: has errors, just not found

--- cmd/calculate.go
+++ cmd/calculate.go
@@ -6,7 +6,9 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"regexp"
 
+	"github.com/j34sy/ToolE/pkg/maths"
 	"github.com/spf13/cobra"
 )
 
@@ -25,22 +27,31 @@ var calculateCmd = &cobra.Command{
 	- Modulus (%)
 	- Exponentiation (^)
 	- nth Root (|)
-	- Factorial (!)
 
 	Each operation is performed on two numbers, so you have to use paranthesis to group operations.
 	Example: calculate 2+3*4 is undefined, but calculate (2+3)*4 is 20 and calculate 2+(3*4) is 14.
 	You can also use the flags to specify the base of the numbers.
 	Available flags are:
-	- --decimal (-d)
-	- --hexadecimal (-h)
-	- --octal (-o)
-	- --binary (-b)
+	- --decimal (-D) [default]
+	- --hexadecimal (-H)  [case-insensitive]
+	- --octal (-O)
+	- --binary (-B)
 
-	Example: calculate --hexadecimal (a^3) + (8|3) is 3EA
+	Example: calculate --hexadecimal (a^3) + (8|3) is 3EA (1002 in decimal)
+	Please Note:
+	- The numbers are considered decimal by default.
+	- Only one flag is allowed.
+	- Decimal values can be floats except for factorial calculations (only integers).
+	- The nth root can be expressed with exponentiation and factorial as well.
+	- Especially the non-decimal calculations are not as accurate as the decimal calculations due to rounding errors.
+	- Do not use spaces between characters
+
+	Examples:
+	- calculate 2+3
+	- calculate 2+(3*4)
+	- calculate --hexadecimal (a^3)+(8|3)
 	`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("calculate called")
-
 		decimal, _ := cmd.Flags().GetBool("decimal")
 		hexadecimal, _ := cmd.Flags().GetBool("hexadecimal")
 		octal, _ := cmd.Flags().GetBool("octal")
@@ -54,22 +65,99 @@ var calculateCmd = &cobra.Command{
 			}
 		}
 
+		input := splitCalculate(args[0])
+
 		if count > 1 {
 			fmt.Println("Only one flag is allowed")
 			os.Exit(1)
 		}
 
+		if len(args) == 0 {
+			fmt.Println("No term provided")
+			os.Exit(1)
+		}
+
 		if hexadecimal {
-			fmt.Println("Hexadecimal")
+			checkHex(input)
+			println(maths.HexCalc(input))
+
 		} else if octal {
-			fmt.Println("Octal")
+			checkOct(input)
+			println(maths.OctalCalc(input))
 		} else if binary {
-			fmt.Println("Binary")
+			checkBin(input)
+			println(maths.BinaryCalc(input))
 		} else {
-			fmt.Println("No flag or decimal flag")
-			fmt.Println("Default: Decimal")
+			checkDec(input)
+			println(maths.DecimalCalc(input))
 		}
 	},
+}
+
+func checkHex(input []string) {
+	for _, v := range input {
+		for _, c := range v {
+			if !checkValList(hexDigits, string(c)) && !checkValList(operatorsChars, string(c)) {
+				fmt.Println("Invalid character in hexadecimal")
+				os.Exit(1)
+			}
+		}
+	}
+}
+
+func checkOct(input []string) {
+	for _, v := range input {
+		for _, c := range v {
+			if !checkValList(octDigits, string(c)) && !checkValList(operatorsChars, string(c)) {
+				fmt.Println("Invalid character in octal")
+				os.Exit(1)
+			}
+		}
+	}
+}
+
+func checkBin(input []string) {
+	for _, v := range input {
+		for _, c := range v {
+			if !checkValList(binDigits, string(c)) && !checkValList(operatorsChars, string(c)) {
+				fmt.Println("Invalid character in binary")
+				os.Exit(1)
+			}
+		}
+	}
+}
+
+func checkDec(input []string) {
+	for _, v := range input {
+		for _, c := range v {
+			if !checkValList(decimalDigits, string(c)) && !checkValList(operatorsChars, string(c)) {
+				fmt.Println("Invalid character in decimal")
+				os.Exit(1)
+			}
+		}
+	}
+}
+
+var hexDigits = []string{"a", "b", "c", "d", "e", "f", "A", "B", "C", "D", "E", "F", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+var octDigits = []string{"0", "1", "2", "3", "4", "5", "6", "7"}
+var binDigits = []string{"0", "1"}
+var decimalDigits = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+var operatorsChars = []string{"+", "-", "*", "/", "%", "^", "|", "(", ")"}
+
+func splitCalculate(input string) []string {
+	re := regexp.MustCompile(`([^+\-*/%^|()]+)`)
+	parts := re.FindAllString(input, -1)
+	return parts
+}
+
+func checkValList(list []string, val string) bool {
+	// Add your code here
+	for _, v := range list {
+		if v == val {
+			return true
+		}
+	}
+	return false
 }
 
 func init() {
@@ -83,8 +171,8 @@ func init() {
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	calculateCmd.Flags().BoolP("decimal", "d", false, "Understand the numbers as decimal")
-	calculateCmd.Flags().BoolP("hexadecimal", "h", false, "Understand the numbers as hexadecimal")
-	calculateCmd.Flags().BoolP("octal", "o", false, "Understand the numbers as octal")
-	calculateCmd.Flags().BoolP("binary", "b", false, "Understand the numbers as binary")
+	calculateCmd.Flags().BoolP("decimal", "D", false, "Understand the numbers as decimal")
+	calculateCmd.Flags().BoolP("hexadecimal", "H", false, "Understand the numbers as hexadecimal")
+	calculateCmd.Flags().BoolP("octal", "O", false, "Understand the numbers as octal")
+	calculateCmd.Flags().BoolP("binary", "B", false, "Understand the numbers as binary")
 }
