182ec9be1c83eb656392a56a3ebefd27dbbc76b4
j34sy <j34sy@proton.me>
2024-07-19 18:18:27 +0200

    feat: setup support for binary, octal, decimal and hex operations

--- cmd/calculate.go
+++ cmd/calculate.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -21,6 +22,35 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("calculate called")
+
+		decimal, _ := cmd.Flags().GetBool("decimal")
+		hexadecimal, _ := cmd.Flags().GetBool("hexadecimal")
+		octal, _ := cmd.Flags().GetBool("octal")
+		binary, _ := cmd.Flags().GetBool("binary")
+
+		flags := []bool{decimal, hexadecimal, octal, binary}
+		count := 0
+		for _, flag := range flags {
+			if flag {
+				count++
+			}
+		}
+
+		if count > 1 {
+			fmt.Println("Only one flag is allowed")
+			os.Exit(1)
+		}
+
+		if hexadecimal {
+			fmt.Println("Hexadecimal")
+		} else if octal {
+			fmt.Println("Octal")
+		} else if binary {
+			fmt.Println("Binary")
+		} else {
+			fmt.Println("No flag or decimal flag")
+			fmt.Println("Default: Decimal")
+		}
 	},
 }
 
@@ -35,5 +65,8 @@ func init() {
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// calculateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	calculateCmd.Flags().BoolP("decimal", "d", false, "Understand the numbers as decimal")
+	calculateCmd.Flags().BoolP("hexadecimal", "h", false, "Understand the numbers as hexadecimal")
+	calculateCmd.Flags().BoolP("octal", "o", false, "Understand the numbers as octal")
+	calculateCmd.Flags().BoolP("binary", "b", false, "Understand the numbers as binary")
 }
